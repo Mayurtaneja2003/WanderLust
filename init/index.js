de61cc5859a132953c7569b2b2a6b8b93a48b1e7
Mayurtaneja2003 <mayurtaneja7@gmail.com>
2025-04-12 18:46:13 +0530

    Update index.js for MongoDB connection or other changes

--- init/index.js
+++ init/index.js
@@ -1,8 +1,11 @@
+require("dotenv").config({path: "../.env"});
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const  Listing = require("../models/listing.js");
 
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
+
+console.log("MongoDB Connection String:", dbUrl);
 
 main()
     .then(()=>{
@@ -13,12 +16,12 @@ main()
     });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 const initDB=async()=>{
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj)=>({...obj, owner:"67ef586a2163b680b58ecb21"}));
+    initData.data = initData.data.map((obj)=>({...obj, owner:"67ef586a2163b680b58ecb21", geometry: { type: "Point", coordinates: [77.1025, 28.7041] }}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
